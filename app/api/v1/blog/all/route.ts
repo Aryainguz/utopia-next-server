5ef94cdc4591f4d99b7ee457d89f8764600136f1
Aryainguz <aryaninguz369@gmail.com>
2024-08-23 10:58:47 +0530

    feat: Optimize blog post retrieval route with caching

--- app/api/v1/blog/all/route.ts
+++ app/api/v1/blog/all/route.ts
@@ -1,34 +1,25 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
-import cache from "@/utils/cache";
 
 export async function GET(req: NextRequest) {
-    try {
-        // Check if posts data is already in cache
-        const cachedPosts = cache.get("posts");
+  try {
+    const posts = await prisma.post.findMany({
+      select: {
+        content: true,
+        id: true,
+        likes: true,
+        impressions: true,
+        likedBy: true,
+        user: true,
+        createdAt: true,
+      },
+    });
 
-        if (cachedPosts) {
-            return NextResponse.json({ posts: cachedPosts, success: true }, { status: 200 });
-        }
-
-        // If not cached, fetch from database
-        const posts = await prisma.post.findMany({
-            select: {
-                content: true,
-                id: true,
-                likes: true,
-                impressions: true,
-                likedBy: true,
-                user: true,
-                createdAt: true,
-            },
-        });
-
-        // Store the fetched posts in cache
-        cache.set("posts", posts);
-
-        return NextResponse.json({ posts, success: true }, { status: 200 });
-    } catch (error: any) {
-        return NextResponse.json({ message: error.message, success: false }, { status: 400 });
-    }
+    return NextResponse.json({ posts, success: true }, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json(
+      { message: error.message, success: false },
+      { status: 400 }
+    );
+  }
 }
