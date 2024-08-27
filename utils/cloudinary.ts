51fc01e55868ed49a33b8e059247b7d6d71e661c
Aryainguz <aryaninguz369@gmail.com>
2024-08-27 11:14:35 +0530

    refactor: Update cloudinary configuration to use environment variables

--- utils/cloudinary.ts
+++ utils/cloudinary.ts
@@ -2,9 +2,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-    cloud_name: 'dlauprc9e',
-    api_key: '398767317679783',
-    api_secret: 'O8Cou8B1HMYE2UH5_tfAOag68uQ',
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
     secure: true,
   });
 
